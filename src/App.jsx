87289d4b7723edacc049a6ce26689405909d79c0
Subx1s0o <nazarsabardin@gmail.com>
2024-05-27 20:02:39 +0300

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ContactForm from "./Components/ContactForm/ContactForm";
 import ContactList from "./Components/ContactList/ContactList";
 import SearchBox from "./Components/SearchBox/SearchBox";
 import { useState } from "react";
+
 function App() {
   const [users, setUsers] = useState([
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
@@ -10,6 +11,7 @@ function App() {
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
   ]);
+
   const [inputValue, setInputValue] = useState("");
 
   const handleChange = (evt) => {
@@ -17,20 +19,22 @@ function App() {
   };
 
   const deleteUser = (userId) => {
-    setUsers((prevUsers) => {
-      return prevUsers.filter((user) => user.id !== userId);
-    });
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
   };
 
-  const visibleUsers = users.filter((user) => {
-    return user.name.toLowerCase().includes(inputValue.toLowerCase());
-  });
+  const addUser = (newUser) => {
+    setUsers((prevUsers) => [...prevUsers, newUser]);
+  };
+
+  const visibleUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
 
   return (
     <div className="container">
       <div>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm onAdd={addUser} />
         <SearchBox inputValue={inputValue} onChange={handleChange} />
         <ContactList users={visibleUsers} onDelete={deleteUser} />
       </div>
--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,3 +1,24 @@
-export default function ContactForm() {
-  return <div>ContactForm</div>;
+import { Formik, Field, Form } from "formik";
+import { nanoid } from "nanoid";
+
+export default function ContactForm({ onAdd }) {
+  const handleSubmit = (values, { resetForm }) => {
+    onAdd({
+      id: nanoid(),
+      name: values.name,
+      number: values.phone,
+    });
+    resetForm();
+  };
+  return (
+    <div>
+      <Formik initialValues={{ name: "", phone: "" }} onSubmit={handleSubmit}>
+        <Form>
+          <Field type="text" name="name" />
+          <Field type="yext" name="phone" />
+          <button type="submit">add </button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
