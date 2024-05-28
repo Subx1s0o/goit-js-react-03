6c34b0bfef777c892341df16f1d7a6ea3755f493
Subx1s0o <nazarsabardin@gmail.com>
2024-05-28 21:11:36 +0300

    push

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "modern-normalize/modern-normalize.css";
+import "modern-normalize";
 import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
