2ce2d5b6d10d74b2fe3491796a3a57c01e1d8f03
Subx1s0o <nazarsabardin@gmail.com>
2024-05-28 14:46:57 +0300

    push

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,5 +1,21 @@
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { nanoid } from "nanoid";
+import * as Yup from "yup";
+
+import css from "./contactForm.module.css";
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(15, "Too Long!")
+    .required("Required")
+    .matches(/^[A-Za-z\s]+$/, "Name can only contain english letters"),
+  phone: Yup.string()
+    .min(4, "Too Short!")
+    .max(30, "Too Long!")
+    .required("Required")
+    .matches(/^\d+$/, "Phone must be a number"),
+});
 
 export default function ContactForm({ onAdd }) {
   const handleSubmit = (values, { resetForm }) => {
@@ -12,12 +28,38 @@ export default function ContactForm({ onAdd }) {
   };
   return (
     <div>
-      <Formik initialValues={{ name: "", phone: "" }} onSubmit={handleSubmit}>
-        <Form>
-          <Field type="text" name="name" />
-          <Field type="yext" name="phone" />
-          <button type="submit">add </button>
-        </Form>
+      <Formik
+        initialValues={{ name: "", phone: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ isValid }) => (
+          <Form className={css.form}>
+            <label className={css.label} htmlFor="name">
+              Name
+              <Field id="name" type="text" name="name" />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={css.errors}
+              />
+            </label>
+
+            <label className={css.label} htmlFor="phone">
+              Phone
+              <Field id="phone" type="text" name="phone" />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className={css.errors}
+              />
+            </label>
+
+            <button type="submit" disabled={!isValid}>
+              Add
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
